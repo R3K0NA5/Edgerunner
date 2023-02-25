abe923994bba4253cb42557391e7aa9bb031ae8e
R3K0NA5 <120364068+R3K0NA5@users.noreply.github.com>
2023-02-25 17:23:39 +0200

    Setting up project.

---
+++ laravel_failai/database/seeders/AdminUserSeed.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::firstOrCreate(
+            [
+                'email' => 'admin@example.com',
+            ],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('admin'),
+            ]
+        );
+
+    }
+}
